6f834c2c31a532f8fb0e10932d8b31caec799344
Thorsten Zoerner <me@thorsten-zoerner.com>
2022-04-03 14:11:05 +0200

    v0.5.5

--- receive.js
+++ receive.js
@@ -7,17 +7,22 @@ module.exports = function(RED) {
 
         const poll = async function() {
           try {
-            const responds = await axios.get(config.server+"message?token="+config.token+"&limit=1",msg.payload);
+            console.log("Poll",config.server+"message?token="+config.token+"&limit=1");
+            const responds = await axios.get(config.server+"message?token="+config.token+"&limit=1");
             let lastMsgId = storage.get("lastId");
             const msgs = responds.data.messages;
             for(let i=0;i<msgs.length;i++) {
               if(msgs[i].id !== lastMsgId) {
                 storage.set("lastId",msgs[i].id);
-                node.send(msgs[i]);
+                node.send({payload:msgs[i]});
               }
             }
           } catch(e) {console.log(e);}
         }
+        node.on('input', async function(msg) {
+          poll();
+        });
+
         setInterval(poll,60000);
     }
     RED.nodes.registerType("Gotify-Receive",Receive);
